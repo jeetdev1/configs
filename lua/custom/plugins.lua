f879780f73f81351644add24a83db7e7a68e3032
Jeet Mukherjee <jeetmukherjee100@gmil.com>
2024-03-05 08:42:22 +0530

    setting up for node.js mainly, not tested react yet

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,28 @@
+local plugins = {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettier",
+        "typescript-language-server"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+
+return plugins
